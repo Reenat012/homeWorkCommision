7777e45b02d1d8bb632a232563223f5300c0eb6a
Reenat <reenat056@yandex.ru>
2023-12-03 17:32:51 +0300

    change main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,62 @@
 fun main() {
     val typeCard = "MasterCard"
-    val sumRemittanceMonthAgo = 300
-    val sumRemittanceNew = 100
+    val sumRemittanceMonthAgo = 3_000_000
+    val sumRemittanceNew = 120_000
     printSumRemmittance(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) //расчет комиссии
 }
 
-//проверяем входит ли сумма переводов в лимит
-fun isExceedLimit(typeCard: String, sumRemittanceMonthAgo: Int, sumRemittanceNew: Int): Boolean {
+//проверяем входит ли сумма переводов в лимит в сутки
+fun isExceedLimitDay(typeCard: String, sumRemittanceNew: Int): Boolean {
     return when (typeCard) {
-        "MasterCard", "Maestro", "Visa", "Мир" -> if (sumRemittanceMonthAgo + sumRemittanceNew <= 60_000) true else false
+        "MasterCard", "Maestro", "Visa", "Мир" -> if (sumRemittanceNew in 0..150_000) true else false
+        "VK Pay" -> if (sumRemittanceNew in 0..15_000) false else true
+        else -> false
+    }
+}
+
+//проверяем входит ли сумма переводов в лимит в месяц
+fun isExceedLimitMonth(typeCard: String, sumRemittanceMonthAgo: Int, sumRemittanceNew: Int): Boolean {
+    return when (typeCard) {
+        "MasterCard", "Maestro", "Visa", "Мир" -> if (sumRemittanceMonthAgo + sumRemittanceNew <= 600_000) true else false
         "VK Pay" -> if (sumRemittanceMonthAgo + sumRemittanceNew <= 40_000) true else false
         else -> false
     }
 }
 
-//рассчитываем сумму комиссии
+//рассчитываем сумму комиссии, если превышены лимиты возвращает -1 или -2
 fun calcCommission(typeCard: String = "VK Pay", sumRemittanceMonthAgo: Int = 0, sumRemittanceNew: Int): Int {
-    return if (isExceedLimit(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)) {
-        when (typeCard) {
-            "MasterCard", "Maestro" -> if (300 <= sumRemittanceNew && sumRemittanceNew <= 75000) 0
-            else (sumRemittanceNew * 0.06).toInt() + 20
+    return if (isExceedLimitDay(typeCard, sumRemittanceNew)) {
+        if (isExceedLimitMonth(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)) {
+            when (typeCard) {
+                "MasterCard", "Maestro" -> if ((sumRemittanceMonthAgo + sumRemittanceNew) in 300..75_000) 0
+                else (sumRemittanceNew * 0.06).toInt() + 20
 
-            "Visa", "Мир" -> if ((sumRemittanceNew * 0.75) < 35) 35
-            else (sumRemittanceNew * 0.75).toInt()
+                "Visa", "Мир" -> if ((sumRemittanceNew * 0.75) < 35) 35
+                else (sumRemittanceNew * 0.75).toInt()
 
-            else -> 0
-        }
-    } else return -1
+                else -> 0
+            }
+        } else return -1
+    } else -2
 }
 
-fun sumRemittanceWithCommision(typeCard: String = "VK Pay", sumRemittanceMonthAgo: Int = 0, sumRemittanceNew: Int): Int {
+//суммируем разовый перевод и комиссию, если вывод -1 или -2 исчерпаны лимиты
+fun sumRemittanceWithCommision(
+    typeCard: String = "VK Pay",
+    sumRemittanceMonthAgo: Int = 0,
+    sumRemittanceNew: Int
+): Int {
     return if (calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -1) -1
+    else if (calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -2) -2
     else sumRemittanceNew + calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)
 }
 
+//вывод перевода с комиссией, с условием превышения лимита
 fun printSumRemmittance(typeCard: String, sumRemittanceMonthAgo: Int, sumRemittanceNew: Int) {
     if (sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -1)
         println("Лимит переводов в этом месяце исчерпан")
+    else if (sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -2)
+        println("Лимит переводов в сутки исчерпан")
     else println(sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew))
 }
 
